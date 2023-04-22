3adddaef6fb12f1d8c0c06b6e1b857bcfc950964
Adithya <jhadith88@gmail.com>
2023-04-22 17:00:58 +0530

    added stuffs for the level

--- components/Level.js
+++ components/Level.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { RigidBody } from '@react-three/rapier';
 
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 
 import * as THREE from 'three';
 
@@ -36,6 +37,19 @@ function BlockStart({ position = [0, 0, 0] }) {
 
 function BlockSpinner({ position = [0, 0, 0] }) {
   const obstacle = useRef();
+
+  const [speed] = useState(
+    () => (Math.random() + 0.4) * (Math.random() < 0.5 ? -1 : 1)
+  );
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const rotation = new THREE.Quaternion();
+
+    rotation.setFromEuler(new THREE.Euler(0, time * speed, 0));
+    obstacle.current.setNextKinematicRotation(rotation);
+  });
+
   return (
     <group position={position}>
       {/* floor */}
@@ -52,6 +66,52 @@ function BlockSpinner({ position = [0, 0, 0] }) {
         position={[0, 0.3, 0]}
         restitution={0.2}
         friction={0}
+        ref={obstacle}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={obstacleMaterial1}
+          scale={[3.5, 0.3, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  );
+}
+
+function BlockLimbo({ position = [0, 0, 0] }) {
+  const obstacle = useRef();
+
+  const [timeOffset] = useState(() => Math.random() * Math.PI * 2);
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const y = Math.sin(time + timeOffset) + 1.15;
+    obstacle.current.setNextKinematicTranslation({
+      x: position[0],
+      y: position[1] + y,
+      z: position[2],
+    });
+  });
+
+  return (
+    <group position={position}>
+      {/* floor */}
+      <mesh
+        geometry={boxGeometry}
+        material={floorMaterial2}
+        scale={[4, 0.2, 4]}
+        position={[0, -0.1, 0]}
+        receiveShadow
+      />
+
+      <RigidBody
+        type="kinematicPosition"
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+        ref={obstacle}
       >
         <mesh
           geometry={boxGeometry}
@@ -68,8 +128,9 @@ function BlockSpinner({ position = [0, 0, 0] }) {
 function Level() {
   return (
     <>
-      <BlockStart position={[0, 0, 4]} />
-      <BlockSpinner position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 8]} />
+      <BlockSpinner position={[0, 0, 4]} />
+      <BlockLimbo position={[0, 0, 0]} />
     </>
   );
 }
